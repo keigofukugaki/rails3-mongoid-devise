89e9aeacb9f11f4f9667fd53b4a45a08a14edc9b
Keigo Fukugaki <keigofukugaki@gmail.com>
2013-05-13 19:28:42 +0900

    Configuration Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@
 puts 'DEFAULT USERS'
 user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
+user2 = User.create! :name => 'user2', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+puts 'user: ' << user2.name
